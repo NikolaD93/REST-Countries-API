d562379da7fc50a94ed88e79eb12e44af5b7efef
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-01-26 15:21:30 +0100

    added back to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import MoonIcon from "../assets/MoonIcon";
 import SunIcon from "../assets/SunIcon";
 import ModeContext from "../context/ModeContext";
@@ -17,7 +18,7 @@ const Navbar = () => {
       } px-[20px] sm:px-[80px] py-[25px] flex justify-between items-center sticky top-0 left-0 w-[100%] z-10 shadow-md`}
     >
       <div className="left">
-        <h1 className="sm:text-2xl font-[800]">Where in the world?</h1>
+        <Link to="/" className="sm:text-2xl font-[800]">Where in the world?</Link>
       </div>
       <div
         onClick={handleMode}
